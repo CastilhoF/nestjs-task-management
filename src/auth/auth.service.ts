78683b873e1cb97eb940c94678f76476479de64e
CastilhoF <castilho.wylde@gmail.com>
2021-12-28 09:02:05 -0300

    add signin method and hashed password compare

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './users.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,5 +13,16 @@ export class AuthService {
 
   async singUp(authCredentials: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentials);
+  }
+
+  async singIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.usersRepository.findOne({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'success';
+    } else {
+      throw new UnauthorizedException('Please check your login credentials');
+    }
   }
 }
